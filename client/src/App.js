932a030703a3aeb92c97808668a3d58fa9eb0190
WebMats <lorlando127@gmail.com>
2019-01-22 15:39:36 -0500

    connected react-router and added proper routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
@@ -20,9 +21,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Switch>
+          <Route path="/" exact component={MovieList} />
+          <Route path="/movies/:movieId" component={Movie} />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
